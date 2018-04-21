b44cd81adc61f3e4c0859aa7edfc567df2a8ac46
Citillara <Citillara@users.noreply.github.com>
2018-04-21 22:33:08 +0200

    Added methods to support the new keep alive feedback/trigger

--- Main/Bot/Bot.cs
+++ Main/Bot/Bot.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Twitch;
 
@@ -15,6 +17,11 @@ namespace CitiBot.Main
         private string m_Password;
         private PluginManager m_PluginManager;
         private TwitchClient m_TwitchClient;
+        private HttpListener m_HttpListener;
+        private Thread m_HttpListenerThread;
+        private bool m_HttpListenerThreadLoop;
+        private ManualResetEvent m_HttpListenerReset;
+        private DateTime m_StartTime;
 
         private int m_ReconnectAttempts = 0;
         private const int MAX_RECONNECT_ATTEMPS = 3;
@@ -31,6 +38,9 @@ namespace CitiBot.Main
 
         public void Start()
         {
+            m_HttpListenerThread = new Thread(new ThreadStart(HttpListenerThread));
+            m_HttpListenerThread.Start();
+
             m_TwitchClient = new TwitchClient(m_Name, m_Password);
             m_TwitchClient.OnDisconnect += m_TwitchClient_OnDisconnect;
             m_TwitchClient.OnJoin += m_TwitchClient_OnJoin;
@@ -44,7 +54,78 @@ namespace CitiBot.Main
                 m_TwitchClient.LogLevel = Twitch.Models.MessageLevel.Info;
             m_TwitchClient.AutoDetectSendWhispers = true;
             m_TwitchClient.Connect();
+            m_StartTime = DateTime.Now;
         }
+
+        void HttpListenerThread()
+        {
+            string httplistenerport = System.Configuration.ConfigurationManager.AppSettings["httplistenerport"];
+            m_HttpListener = new HttpListener();
+            m_HttpListener.Prefixes.Add("http://localhost:" + httplistenerport + "/");
+            m_HttpListener.Start();
+            m_HttpListenerThreadLoop = true;
+            m_HttpListenerReset = new ManualResetEvent(false);
+            while (m_HttpListenerThreadLoop)
+            {
+                m_HttpListenerReset.Reset();
+                IAsyncResult result = m_HttpListener.BeginGetContext(new AsyncCallback(ListenerCallback), this);
+                m_HttpListenerReset.WaitOne();
+            }
+        }
+
+        public static void ListenerCallback(IAsyncResult result)
+        {
+            Bot bot = (Bot)result.AsyncState;
+            // Call EndGetContext to complete the asynchronous operation.
+            HttpListenerContext context = bot.m_HttpListener.EndGetContext(result);
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            bool noBody = false;
+
+            StringBuilder sb = new StringBuilder();
+
+            if (request.Url.AbsoluteUri.EndsWith("status"))
+            {
+                sb.Append("Start time : ");
+                sb.AppendLine(bot.m_StartTime.ToString());
+                sb.Append("Last keep alive : ");
+                sb.AppendLine(bot.m_TwitchClient.LastKeepAlive.ToString());
+                sb.Append("Next keep alive : ");
+                sb.AppendLine(bot.m_TwitchClient.NextKeepAlive.ToString());
+            }
+            else if(request.Url.AbsoluteUri.EndsWith("triggerKeepAlive"))
+            {
+                sb.AppendLine("Triggering keep alive");
+                bot.m_TwitchClient.TriggerNextKeepAlive();
+            } 
+            else
+            {
+                noBody = true;
+                response.StatusCode = 404;
+            }
+
+            if (noBody)
+            {
+                response.OutputStream.Close();
+            }
+            else
+            {
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(sb.ToString()); ;
+                // Get a response stream and write the response to it.
+                response.ContentType = " text/plain";
+                response.ContentLength64 = buffer.Length;
+
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                // You must close the output stream.
+                output.Close();
+            }
+            bot.m_HttpListenerReset.Set();
+        }
+
 
         void m_TwitchClient_OnNotice(TwitchClient sender, Twitch.Models.TwitchNotice args)
         {
